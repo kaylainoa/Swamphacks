070a1510f4359d70aabbd4e26d04098b1e55e7d9
Adora <adoraloin@gmail.com>
2025-01-25 13:57:06 -0500

    added clinic page

--- src/Clinic.js
+++ src/Clinic.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import './Clinic.css';
+
 
 export const Clinic = () => {
   return (
     <div>
         <h1>Clinic</h1>
-        </div>
-  )
-}
+        <div className="button-container">
+        <button className="button">Find Clinics Near Me</button>
+        <button className="button">Contact</button>
+      </div>
 
-export default Clinic
+        <div className="clinic-info">
+        <div>Name: General Healthcare Clinic</div>
+        <div>Phone: 123-456-7890</div>
+        <div>Rating: 4.5 / 5</div>
+        <div>Services: General Healthcare, Vaccinations, Lab Tests</div>
+        <div>Hours: Mon-Fri: 9:00 AM - 5:00 PM</div>
+        <div>
+          Website: <a href="http://example.com" className="website-link">Visit Website</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Clinic;
