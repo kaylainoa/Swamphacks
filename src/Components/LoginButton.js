b67db554d22883ee3e5ac3ce4c13fd3f41fca156
kaylainoa <kinoa718@gmail.com>
2025-01-26 04:51:58 -0500

    page updates

--- src/Components/LoginButton.js
+++ src/Components/LoginButton.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
-  return (
-    <button onClick={() => loginWithRedirect()} className="login-button">
-      Log In
-    </button>
-  );
-};
+  const navigate = useNavigate(); // useHistory() for v5 
+  const handleLogin = () => { 
+    loginWithRedirect({
+     redirectUri: window.location.origin + "/loggedin" // Explicit redirect URI after login 
+     }); 
+    }; 
+  return <button onClick={handleLogin}>Log In</button>; };
+  
 
 export default LoginButton;
